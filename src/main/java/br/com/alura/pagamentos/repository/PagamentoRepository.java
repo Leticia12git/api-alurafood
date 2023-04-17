7cf73347e22c3853fc2291c9eb573072200745a1
Leticia12 <leticia.silva@arphoenix.com.br>
2023-04-16 22:05:57 -0300

    Adicionando os pacotes repositorio e dto do projeto

---
+++ src/main/java/br/com/alura/pagamentos/repository/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package br.com.alura.pagamentos.repository;
+
+import br.com.alura.pagamentos.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+}
